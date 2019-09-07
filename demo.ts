fe6de2b4227e5f62e4177be5e95e31fec3675849
bLue <tbdblue@gmail.com>
2019-09-07 17:37:54 +0800

    U: move types to exported namespace

---
+++ demo.ts
@@ -0,0 +1,95 @@
+import srk from '.';
+
+const ranklist: srk.Ranklist = {
+  type: 'standard',
+  version: '0.1.0',
+  contest: {
+    title: 'ACM ICPC World Finals 2018',
+    startAt: '2018-04-19T09:51:00+08:00',
+    duration: [5, 'h'],
+    frozenDuration: [1, 'h'],
+    link: 'https://icpc.baylor.edu/scoreboard/',
+  },
+  problems: [
+    {
+      title: 'Catch the Plane',
+      alias: 'A',
+      link: 'https://open.kattis.com/problems/catch',
+      statistics: {
+        accepted: 111,
+        submitted: 268,
+      },
+      style: {
+        backgroundColor: '#58a2d1',
+      },
+    },
+    {
+      title: 'Comma Sprinkler',
+      alias: 'B',
+      link: 'https://open.kattis.com/problems/comma',
+      statistics: {
+        accepted: 152,
+        submitted: 265,
+      },
+      style: {
+        backgroundColor: '#fc6d3e',
+      },
+    },
+  ],
+  series: [
+    {
+      title: 'Rank',
+      segments: [
+        {
+          title: 'Gold Medalist',
+          count: 4,
+          style: 'gold',
+        },
+        {
+          title: 'Silver Medalist',
+          count: 4,
+          style: 'silver',
+        },
+        {
+          title: 'Bronze Medalist',
+          count: 4,
+          style: 'bronze',
+        },
+      ],
+    }
+  ],
+  rows: [
+    {
+      ranks: [
+        {
+          rank: 1,
+          segmentIndex: 0,
+        },
+      ],
+      user: {
+        name: 'Moscow State University',
+        avatar: '',
+      },
+      score: {
+        value: 9,
+        time: [1427, 'min'],
+      },
+      statuses: [
+        {
+          result: 'AC',
+          time: [79, 'min'],
+          tries: 1,
+        },
+        {
+          result: 'AC',
+          time: [87, 'min'],
+          tries: 1,
+        },
+      ],
+    },
+  ],
+  sorter: {
+    algorithm: 'ICPC',
+    config: {},
+  },
+};
